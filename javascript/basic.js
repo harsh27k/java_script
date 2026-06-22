ca764ee4aedec41103dc8b207eb5fb4d00a63e91
harsh27k <harsh27p004@gmail.com>
2026-06-22 09:47:26 +0000

    DATATYPE

---
+++ javascript/basic.js
@@ -0,0 +1,23 @@
+const accountid=123343
+let accountemail="harsh27p004@gmail.com"
+var accountname="HARSH"
+accountcity="delhi"
+console.table([accountemail,accountid,accountname,accountcity])
+
+// var keyword not use generally
+
+// UNDEFINED IS A DATATYPE ITSEELF
+console.log(typeof undefined)
+// but NULL IS A OBJECT 
+console.log(typeof null)
+/* TYPE OD DATATYPE IN JS
+
+1.STRING Eg let name=""
+2.BOOLEAN let isvalue= true/false
+3.NUMBER let age = 21
+4. BIGINT 
+5. SYMBOLE "special datatype"
+6.NULL => STANDALONE VALUE 
+7.UNDEFINED 
+8.OBJCET
+*/
